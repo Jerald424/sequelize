9e854b7b2114538db556c5cf1103387c401b3e41
Jerald <jerald@dbcyelagiri.edu.in>
2023-01-08 13:55:54 +0530

    res role

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -3,10 +3,16 @@ const db = require('../sequelize')
 
 router.post('/login', async (req, res) => {
     try {
+        const { role_id } = req.body;
         const { user_name, password } = req.body;
         if (!(Boolean(user_name) && Boolean(password))) return res.status(400).send('required field missing')
 
-        const role = await db.Role.findAll()
+        const roles = await db.Role.findAll({
+            where: {
+                id: role_id
+            }
+        })
+        res.json({ role: roles })
 
 
     } catch (error) {
