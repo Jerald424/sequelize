8f48514c29d14b7a6b5162f2f7a803296b1b5ef7
Jerald <jerald@dbcyelagiri.edu.in>
2023-03-10 13:51:44 +0530

    .

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -15,70 +15,71 @@ import Hover from "./styleComponent/Hover";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ children }) => {
-  const { colors } = useColors();
-  const navigate = useNavigate();
-  const { isDark } = ThemeStore.useState();
-  const [toggle, setToggle] = useState(true);
-  const [hover, setHover] = useState(false);
+    const { colors } = useColors();
+    const navigate = useNavigate();
+    const { isDark } = ThemeStore.useState();
+    const [toggle, setToggle] = useState(true);
+    const [hover, setHover] = useState(false);
 
-  const navigationAssets = [
-    { name: "Home", link: "/", icon: BiHome },
-    { name: "Profile", link: "/profile", icon: CgProfile },
-    { name: "Chat", link: "/chat", icon: FiUsers },
-    { name: "Settings", link: "#", icon: IoMdSettings },
-    { name: "Posts", link: "/posts", icon: IoMdChatboxes },
-  ];
+    const navigationAssets = [
+        { name: "Home", link: "/", icon: BiHome },
+        { name: "Profile", link: "/profile", icon: CgProfile },
+        { name: "Chat", link: "/chat", icon: FiUsers },
+        { name: "Settings", link: "#", icon: IoMdSettings },
+        { name: "Posts", link: "/posts", icon: IoMdChatboxes },
 
-  //   _____HANDLE__HOVER_TOGGLE_________
-  const handleHover = (type) => {
-    if (type === "on") {
-      setHover(true);
-      //   setToggle(true);
-    } else {
-      !toggle && setHover(false);
-    }
-  };
+    ];
 
-  //   ___HANDLE__TOGGLE___
-  const handleToggle = () => {
-    setToggle(!toggle);
-    setHover(false);
-  };
-  return (
-    <div className="sidebar-routes">
-      <div onMouseEnter={() => handleHover("on")} onMouseLeave={() => handleHover("off")} className={`total-sidebar ${hover && "sidebar-absolute"} ${toggle || hover ? "total-sidebar-active" : "total-sidebar-inactive"} ${isDark ? "bg-dark" : "bg-light"}`} style={{ boxShadow: `2px 2px 3px ${colors?.popupBg}` }}>
-        <div className="sidebar-toggle-part" onClick={handleToggle}>
-          <div
-            color
-            className="toggle-round dajc"
-            style={{
-              backgroundColor: colors?.backgroundColor,
-              boxShadow: `1px 1px 3px  ${colors?.textSecondary}`,
-              color: colors?.textSecondary,
-            }}
-          >
-            <SlArrowRight size={10} className={!toggle && "toggle-button-off"} />
-          </div>
+    //   _____HANDLE__HOVER_TOGGLE_________
+    const handleHover = (type) => {
+        if (type === "on") {
+            setHover(true);
+            //   setToggle(true);
+        } else {
+            !toggle && setHover(false);
+        }
+    };
+
+    //   ___HANDLE__TOGGLE___
+    const handleToggle = () => {
+        setToggle(!toggle);
+        setHover(false);
+    };
+    return (
+        <div className="sidebar-routes">
+            <div onMouseEnter={() => handleHover("on")} onMouseLeave={() => handleHover("off")} className={`total-sidebar ${hover && "sidebar-absolute"} ${toggle || hover ? "total-sidebar-active" : "total-sidebar-inactive"} ${isDark ? "bg-dark" : "bg-light"}`} style={{ boxShadow: `2px 2px 3px ${colors?.popupBg}` }}>
+                <div className="sidebar-toggle-part" onClick={handleToggle}>
+                    <div
+                        color
+                        className="toggle-round dajc"
+                        style={{
+                            backgroundColor: colors?.backgroundColor,
+                            boxShadow: `1px 1px 3px  ${colors?.textSecondary}`,
+                            color: colors?.textSecondary,
+                        }}
+                    >
+                        <SlArrowRight size={10} className={!toggle && "toggle-button-off"} />
+                    </div>
+                </div>
+                <div className="dfa mt-2" style={{ display: toggle || hover ? "flex" : "none" }}>
+                    <img src={require("assets/logo.png")} className="sidebar-logo me-3" />
+                    <HeadingText style={{ width: "150px" }}>App Name</HeadingText>
+                </div>
+
+                <div style={{ display: toggle || hover ? "block" : "none" }}>
+                    <div className="mt-5">
+                        {navigationAssets?.map((res, i) => (
+                            <Hover className="dajc p-2 mt-1 br-2 cp" key={i + "navigation"} onClick={() => navigate(res?.link)}>
+                                <Icon className="me-4" src={res?.icon} />
+                                <Para className="f1">{res?.name}</Para>
+                            </Hover>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className={`total-routes f1 ${toggle ? "total-routes-sidebar-on" : "total-routes-sidebar-off"} pt-3`}>{children}</div>
         </div>
-        <div className="dfa mt-2" style={{ display: toggle || hover ? "flex" : "none" }}>
-          <img src={require("assets/logo.png")} className="sidebar-logo me-3" />
-          <HeadingText style={{ width: "150px" }}>App Name</HeadingText>
-        </div>
-
-        <div style={{ display: toggle || hover ? "block" : "none" }}>
-          <div className="mt-5">
-            {navigationAssets?.map((res, i) => (
-              <Hover className="dajc p-2 mt-1 br-2 cp" key={i + "navigation"} onClick={() => navigate(res?.link)}>
-                <Icon className="me-4" src={res?.icon} />
-                <Para className="f1">{res?.name}</Para>
-              </Hover>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className={`total-routes f1 ${toggle ? "total-routes-sidebar-on" : "total-routes-sidebar-off"} pt-3`}>{children}</div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar;
